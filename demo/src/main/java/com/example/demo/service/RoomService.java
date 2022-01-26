cfb18ff138eae7ea5da232e6de584588fc0874b2
张经纬 <13248tamako@gmail.com>
2022-01-26 23:31:30 +0800

    删除测试代码

--- demo/src/main/java/com/example/demo/service/RoomService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.bean.Room;
-
-public interface RoomService {
-    Room find(int id);
-}
--- demo/src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.bean.UserBean;
-
-public interface UserService {
-    UserBean loginIn(String name,String password);
-}
