c9a1e9893ec90dc1a5067f0fda6039f4f14ebc61
querryton <querry.ton@qq.com>
2022-01-28 20:04:17 +0800

    添加静态页面：主页面

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PageHeader />
+    <TopMenu />
+    <KeywordSearch />
+    <HouseTable />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageHeader from "./components/PageHeader.vue";
+import TopMenu from "./components/TopMenu.vue";
+import KeywordSearch from "./components/KeywordSearch.vue";
+import HouseTable from "./components/HouseTable.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PageHeader,
+    TopMenu,
+    KeywordSearch,
+    HouseTable,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Table, TableColumn, Button, Popover, Tag, Image, Link, Menu, Submenu, MenuItem, Row, Col, Input, Card } from 'element-ui'
+
+import './assets/icon/iconfont.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Button)
+Vue.use(Popover)
+Vue.use(Tag)
+Vue.use(Image)
+Vue.use(Link)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Input)
+Vue.use(Card)
+
+
 
 new Vue({
   render: h => h(App),
