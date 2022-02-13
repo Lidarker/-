8b49d4b89608056ea5fa115d613abe5bb582274f
westbrook0904 <49219809+westbrook0904@users.noreply.github.com>
2022-02-13 21:54:03 +0800

    Room相关接口跨域

--- demo/src/main/java/com/example/demo/controller/RoomController.java
+++ demo/src/main/java/com/example/demo/controller/RoomController.java
@@ -17,28 +17,34 @@ public class RoomController {
     @CrossOrigin
     public List<Room>getAllRoom(){return roomService.findAllRoom();}
     @RequestMapping(value = "/getRoom",params = {"city"},method = RequestMethod.GET)
+    @CrossOrigin
     public List<Room>getRoomByCity(@RequestParam("city")String city)
     {
         System.out.println(city);
         return roomService.getRoomByCity(city);
     }
     @RequestMapping(value = "/getRoom",params = {"lowmoney","highmoney"},method = RequestMethod.GET)
+    @CrossOrigin
     public List<Room>getRoomByMoney(@RequestParam("lowmoney")int lowMoney,
                                     @RequestParam("highmoney")int highMoney)
     {
         return roomService.getRoomByMoney(lowMoney,highMoney);
     }
     @RequestMapping(value = "/getRoom",params = {"type"},method = RequestMethod.GET)
+    @CrossOrigin
     public List<Room>getRoomByType(@RequestParam("type")String type){
         return roomService.getRoomByType(type);
     }
     @RequestMapping(value = "/getRoom",params = {"word"},method = RequestMethod.GET)
+    @CrossOrigin
     public List<Room>getRoomByDcr(@RequestParam("word")String word)
     {
         return roomService.getRoomByDcr(word);
     }
     @RequestMapping(value = "/getRoom",params = {"id"},method = RequestMethod.GET)
+    @CrossOrigin
     public Room getRoomById(@RequestParam("id")int id){return roomService.getRoomById(id);}
     @RequestMapping(value = "deleteRoom",method = RequestMethod.GET)
+    @CrossOrigin
     public String deleteRoom(@RequestParam("id")int id){return roomService.deleteRoomById(id);}
 }
